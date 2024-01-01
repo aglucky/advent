9dc6135ef15ff1fff66331f5303e7cbfcdd2f315
adam gluck <adam.gluck10@gmail.com>
2024-01-01 18:45:58 -0500

    ad python tree

---
+++ tree.py
@@ -0,0 +1,22 @@
+import argparse
+def tree(size, stump, level=0) :
+    match size:
+        case size if size > level:
+            spaces = " " * (size - level)
+            stars =  "*" * (1 + 2 * level)
+            print(f"{spaces}{stars}{spaces}")
+            tree(size, stump, level+1)
+        case _ :
+            for i in range(stump):
+                spaces = " " * (size)
+                print(f"{spaces}*{spaces}")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("size", help="The size of the tree", type=int)
+    parser.add_argument("--stump", help="The height of the tree stump", type=int, default=3)
+    args = parser.parse_args()
+    tree(args.size, args.stump)
+
+if __name__ == "__main__":
+    main()
